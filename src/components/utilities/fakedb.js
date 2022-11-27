418dd5652ea2f7e38b5ae8a75e7d23511bdc3e3b
asifalam515 <asibulalam515@gmail.com>
2022-11-27 16:23:05 +0600

    object mode adde

--- src/components/utilities/fakedb.js
+++ src/components/utilities/fakedb.js
@@ -1,15 +1,29 @@
 // use local storage to manage cart Data
 const addToDb=(id)=>{
-    const quantity=localStorage.getItem(id)
+    
+    let shoppingCart={}
+// get the shopping cart
+const storedCart=localStorage.getItem('shopping-cart')
+if(storedCart){
+shoppingCart=JSON.parse(storedCart)
+}
+else{
+shoppingCart={}
+
+}
+    // add quantity 
+    const quantity=shoppingCart[id]
     if(quantity){
-        console.log('already exist')
-        const newQuantity=parseInt(quantity)+1;
-        localStorage.setItem(id,newQuantity)
+        
+        const newQuantity=quantity+1;
+        shoppingCart[id]=newQuantity
 
     }
     else{
-        console.log('new item')
-        localStorage.setItem(id,1)
+        
+        shoppingCart[id]=1
     }
+    localStorage.setItem('shopping-cart',JSON.stringify(shoppingCart))
+
 }
 export {addToDb}
